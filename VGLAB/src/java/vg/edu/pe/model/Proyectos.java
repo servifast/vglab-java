938735319b71e35777dcfd119f1712cf048fcf0d
Alvaro Iturrizaga Vargas <aiturrizaga@vallegrande.edu.pe>
2018-04-30 17:28:52 -0500

    Iniciando el proyecto

---
+++ VGLAB/src/java/vg/edu/pe/model/Proyectos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vg.edu.pe.model;
+
+/**
+ *
+ * @author PC28
+ */
+public class Proyectos {
+    
+}
