276b5ab17d5803ac8610b55383c4ffaf880d85ee
jcondori <jcondori@vallegrande.edu.pe>
2018-05-02 11:18:39 -0500

    Conexion a la BD

--- VGLAB/src/java/vg/edu/pe/dao/Dao.java
+++ VGLAB/src/java/vg/edu/pe/dao/Dao.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vg.edu.pe.dao;
 
-/**
- *
- * @author PC28
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Dao {
+
+    private Connection cn;
+
+    public void Conexion() {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            cn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.8.169:1521:XE", "VGLAB", "vallegrande2018");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public void Cerrar() throws SQLException {      //Cerrar la coneccion
+        if (cn != null) {
+            if (cn.isClosed() == false) {
+                cn.close();
+            }
+        }
+    }
+
+    public Connection getCn() {
+        return cn;
+    }
+
+    public void setCn(Connection cn) {
+        this.cn = cn;
+    }
+
+//    public static void main(String[] args) {
+//        Dao dao = new Dao();
+//        dao.Conexion();
+//    }
     
 }
