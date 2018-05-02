092ff297ca2f59e349de50cc913f2d759fd64cd2
Sebastian Leyva Chumpitaz <sleyva@vallegrande.edu.pe>
2018-05-02 15:52:32 -0500

    crud de la tabla persona v2

--- VGLAB/src/java/vg/edu/pe/interfaces/PersonaI.java
+++ VGLAB/src/java/vg/edu/pe/interfaces/PersonaI.java
@@ -5,16 +5,16 @@ import vg.edu.pe.model.PersonaM;
 
 public interface PersonaI {
     
-    //Este metodo sierve para guardar registros en la tabla PERSONA de la base de datos VGLAB
+    //Este metodo sirve para guardar registros en la tabla PERSONA de la base de datos VGLAB
     void guardarPersona(PersonaM persona) throws Exception;
 
-    //Este metodo sierve para modificar registros de la tabla PERSONA de la base de datos VGLAB
+    //Este metodo sirve para modificar registros de la tabla PERSONA de la base de datos VGLAB
     void modificarPersona(PersonaM persona) throws Exception;
 
-    //Este metodo sierve para eliminar un cliente de la tabla PERSONA de la base de datos VGLAB
+    //Este metodo sirve para eliminar una persona de la tabla PERSONA de la base de datos VGLAB
     void eliminarPersona(PersonaM persona) throws Exception;
 
-    //Este metodo sierve para listar los registros de la tabla PERSONA de la base de datos VGLAB
+    //Este metodo sirve para listar los registros de la tabla PERSONA de la base de datos VGLAB
     List<PersonaM> listarPersonas() throws Exception;
     
 }
