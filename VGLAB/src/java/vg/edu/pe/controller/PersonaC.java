89cb2817e5677768346f30bee4aa5e317b4c438d
Sebastian Leyva Chumpitaz <sleyva@vallegrande.edu.pe>
2018-05-02 15:50:21 -0500

    crud de la tabla persona

--- VGLAB/src/java/vg/edu/pe/controller/PersonaC.java
+++ VGLAB/src/java/vg/edu/pe/controller/PersonaC.java
@@ -3,14 +3,92 @@ package vg.edu.pe.controller;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import vg.edu.pe.dao.PersonaD;
+import vg.edu.pe.model.PersonaM;
 
 @Named(value = "personaC")
 @SessionScoped
 public class PersonaC implements Serializable {
+    
+    PersonaM pm = new PersonaM();
+    private PersonaM selectedPersona;
+    private List<PersonaM> lstPersonas;
 
     @PostConstruct
     public void init() {
+        try {
+            listarPersonas();
+        } catch (Exception ex) {
+            Logger.getLogger(PersonaC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void guardarPersona() throws Exception{
+        PersonaD pd;
+        try {
+           pd = new PersonaD();
+           pd.guardarPersona(pm);
+        } catch (Exception e) {
+            throw e;
+        }   
+    }
+    public void modificarPersona() throws Exception{
+        PersonaD pd;
+        try {
+            pd = new PersonaD();
+            pd.modificarPersona(selectedPersona);
+        } catch (Exception e) {
+            throw e;
+        }   
+    }
+    public void eliminarPersona() throws Exception{
+        PersonaD pd;
+        try {
+           pd = new PersonaD(); 
+           pd.eliminarPersona(selectedPersona);
+        } catch (Exception e) {
+            throw e;
+        }   
+    }
+    public void listarPersonas() throws Exception{
+        PersonaD pd;
+        try {
+            pd = new PersonaD();
+            lstPersonas = pd.listarPersonas();
+        } catch (Exception e) {
+            throw e;
+        }   
+    }
+    
+    
+    //Getter and Setter
+
+    public PersonaM getPm() {
+        return pm;
+    }
+
+    public void setPm(PersonaM pm) {
+        this.pm = pm;
+    }
+
+    public PersonaM getSelectedPersona() {
+        return selectedPersona;
+    }
+
+    public void setSelectedPersona(PersonaM selectedPersona) {
+        this.selectedPersona = selectedPersona;
+    }
+
+    public List<PersonaM> getLstPersonas() {
+        return lstPersonas;
+    }
+
+    public void setLstPersonas(List<PersonaM> lstPersonas) {
+        this.lstPersonas = lstPersonas;
     }
     
 }
--- VGLAB/src/java/vg/edu/pe/dao/PersonaD.java
+++ VGLAB/src/java/vg/edu/pe/dao/PersonaD.java
@@ -1,30 +1,81 @@
 package vg.edu.pe.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import vg.edu.pe.interfaces.PersonaI;
 import vg.edu.pe.model.PersonaM;
 
-public class PersonaD extends Dao implements PersonaI{
+public class PersonaD extends Dao implements PersonaI {
 
     @Override
     public void guardarPersona(PersonaM persona) throws Exception {
-        
+        try {
+            this.Conexion();
+            String sql = "INSERT INTO PERSONAS (NOMPER, APEPER, ESTPER) VALUES (?,?,?)";
+            PreparedStatement ps = this.getCn().prepareStatement(sql);
+            ps.setString(1, persona.getNOMPER());
+            ps.setString(2, persona.getAPEPER());
+            ps.setString(3, "A");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
     public void modificarPersona(PersonaM persona) throws Exception {
-        
+        try {
+            this.Conexion();
+            String sql = "UPDATE PERSONAS SET NOMPER = ? ,APEPER = ?,ESTPER = ? WHERE CODPER = ?";
+            PreparedStatement ps = this.getCn().prepareStatement(sql);
+            ps.setString(1, persona.getNOMPER());
+            ps.setString(2, persona.getAPEPER());
+            ps.setString(3, persona.getESTPER());
+            ps.setString(4, persona.getCODPER());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
+
     }
 
     @Override
     public void eliminarPersona(PersonaM persona) throws Exception {
-        
+        try {
+            this.Conexion();
+            String sql = "DELETE FROM PERSONAS WHERE CODPER = ? ";
+            PreparedStatement ps = this.getCn().prepareStatement(sql);
+            ps.setString(1, persona.getCODPER());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
+
     }
 
     @Override
     public List<PersonaM> listarPersonas() throws Exception {
-        return null;
-        
+        List<PersonaM> lista;
+        ResultSet rs;
+        try {
+            String sql = "SELECT * FROM PERSONAS";
+            PreparedStatement ps = this.getCn().prepareStatement(sql);
+            rs = ps.executeQuery();
+            PersonaM pm;
+            lista = new ArrayList<>();
+            while (rs.next()) {
+                pm = new PersonaM();
+                pm.setNOMPER(rs.getString("NOMPER"));
+                pm.setAPEPER(rs.getString("APEPER"));
+                lista.add(pm);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+        return lista;
     }
-    
+
 }
