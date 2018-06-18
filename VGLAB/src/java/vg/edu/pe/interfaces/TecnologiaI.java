a07436a4b3b30f8b2a4ad0f6628cbdf08a6f706a
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-06-17 23:19:55 -0500

    rebase

--- VGLAB/src/java/vg/edu/pe/interfaces/TecnologiaI.java
+++ VGLAB/src/java/vg/edu/pe/interfaces/TecnologiaI.java
@@ -5,7 +5,7 @@ import vg.edu.pe.model.TecnologiaM;
 
 public interface TecnologiaI {
 
-    public void guardarTecnologia(TecnologiaM tecnologia) throws Exception;
+    void guardarTecnologia(TecnologiaM tecnologia) throws Exception;
 
     List<TecnologiaM> listarTecnologia() throws Exception;
 }
